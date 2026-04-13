03d5a23f50e0e28853c40955d842022c9a08266d
Larry <larryle7680@gmail.com>
2026-04-13 13:51:55 -0500

    Finished Menu.java

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
-
+import java.util.Scanner;
 public class Hello {
+    //Creating my scanner
+    static Scanner theScanner = new Scanner(System.in);
+
+    //Main method
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodBye();
+        sayGoodMorning();
+    }
+
+    //Say Hello Method
+    public static void sayHello() {
+        System.out.println("Hello, World");
+    }
+
+    //sayGoodBye Method
+    public static void sayGoodBye() {
+        System.out.println("Goodbye, World");
+    }
+
+    //sayGoodMorning Method
+    public static void sayGoodMorning() {
+        System.out.println("Good, Morning!");
+    }
+
+    //
 }
